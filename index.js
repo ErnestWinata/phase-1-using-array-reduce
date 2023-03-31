55aa7c03f5cd045f780c48a01bf78bb8c3c7dc8f
Ernest <ernestwinata@gmail.com>
2023-03-31 16:52:03 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = addBatteries(batteryBatches);
+function addBatteries(batteryBatches) {
+    return batteryBatches.reduce((accumulator, element) => accumulator + element, 0);
+}
